4fb4d9976d8b6ae10fb1642ca270f247b84f11a0
Ivelina-Zhekova <iveta.zhekova@gmail.com>
2022-09-05 20:32:43 +0300

    bid or liar buttons

--- liars_dice_git.py
+++ liars_dice_git.py
@@ -1,3 +1,4 @@
+from binhex import LINELEN
 from readline import set_completion_display_matches_hook
 import pygame
 import numpy as np 
@@ -17,7 +18,8 @@ pygame.display.set_caption('Liars Dice')
 # define colours 
 WHITE = pygame.Color(255, 255, 255)
 BROWN_SIENNA = pygame.Color(160, 82, 45)
-GREEN = pygame.Color(0, 255, 0)
+BUTTON_TEXT_COLOR = pygame.Color(0, 102, 0) # greenish
+BUTTON_COLOR = pygame.Color(255, 255, 204) # very pale yellow
 PLAYER_ONE_COLOR = pygame.Color(253, 253, 150) # yellow player
 PLAYER_TWO_COLOR = pygame.Color(177, 156, 217) # purple player
 
@@ -67,8 +69,28 @@ def random_dices(CUP_CENTER):
     return dices
 
 
+# possible moves
+def liar_or_bid_window():
+    bid_text_surface = BUTTONS_FONT.render('BID', True, BUTTON_TEXT_COLOR)
+    bid_text_rectangle = bid_text_surface.get_rect()
+    bid_text_rectangle.center = (BUTTON_BID_LOCATION[0] + BUTTON_SIZE[0] / 2, BUTTON_BID_LOCATION[1] + BUTTON_SIZE[1] / 2)
+
+    liar_text_surface = BUTTONS_FONT.render('LIAR', True, BUTTON_TEXT_COLOR)
+    liar_text_rectangle = bid_text_surface.get_rect()
+    liar_text_rectangle.center = (BUTTON_LIAR_LOCATION[0] + BUTTON_SIZE[0] / 2.3, BUTTON_LIAR_LOCATION[1] + BUTTON_SIZE[1] / 2)
+
+    pygame.draw.rect(WIN, BUTTON_COLOR, (BUTTON_BID_LOCATION, BUTTON_SIZE))
+    pygame.draw.rect(WIN, BUTTON_TEXT_COLOR, (BUTTON_BID_LOCATION, BUTTON_SIZE), 4)
+    pygame.draw.rect(WIN, BUTTON_COLOR, (BUTTON_LIAR_LOCATION, BUTTON_SIZE))
+    pygame.draw.rect(WIN, BUTTON_TEXT_COLOR, (BUTTON_LIAR_LOCATION, BUTTON_SIZE), 4)
+
+    WIN.blit(bid_text_surface, bid_text_rectangle)
+    WIN.blit(liar_text_surface, liar_text_rectangle)
+    pygame.display.update()
+
 background()
 first_player_dices = random_dices(PLAYER_ONE_POSITION)
+liar_or_bid_window()
 
 running = True
 
